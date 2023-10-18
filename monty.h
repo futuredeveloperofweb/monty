76127b88dcc877bb54eec72a6bfb58793fca10eb
othmane <othmane.alaouimekkaoui@gmail.com>
2023-10-18 16:31:30 -0700

    test

--- monty.h
+++ monty.h
@@ -1,10 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
+
 #define _GNU_SOURCE
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <stdarg.h>
 
@@ -19,9 +20,9 @@
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
 } stack_t;
 
 /**
@@ -34,37 +35,50 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern stack_t *top;
-typedef void (*o_fct)(stack_t **, unsigned int);
+extern stack_t *head;
+typedef void (*op_func)(stack_t **, unsigned int);
 
-/* file_op.c */
-void open(char *);
-int separat(char *, int, int);
-void _read(FILE *);
-void call_fct(o_fct, char *, char *, int, int);
-void find_fct(char *, char *, int, int);
+/*file operations*/
+void open_file(char *file_name);
+int parse_line(char *buffer, int line_number, int format);
+void read_file(FILE *);
+int len_chars(FILE *);
+void find_func(char *, char *, int, int);
 
-/* stack_operation.c */
-void push(stack_t **, unsigned int);
-void pall(stack_t **, unsigned int);
-void pint(stack_t **, unsigned int);
-void pop(stack_t **, unsigned int);
+/*Stack operations*/
+stack_t *create_node(int n);
+void free_nodes(void);
+void print_stack(stack_t **, unsigned int);
+void add_to_stack(stack_t **, unsigned int);
+void add_to_queue(stack_t **, unsigned int);
 
-/* stack_operation.c */
-void swap(stack_t **, unsigned int);
-void add(stack_t **, unsigned int);
+void call_fun(op_func, char *, char *, int, int);
+
+void print_top(stack_t **, unsigned int);
+void pop_top(stack_t **, unsigned int);
 void nop(stack_t **, unsigned int);
-void sub(stack_t **, unsigned int);
+void swap_nodes(stack_t **, unsigned int);
 
-/* stack_fct.c */
-stack_t *mk_node(int);
-void free_node(void);
-void error_type(int, ...);
-void error_type2(int, ...);
-void add_queue(stack_t **, unsigned int);
+/*Math operations with nodes*/
+void add_nodes(stack_t **, unsigned int);
+void sub_nodes(stack_t **, unsigned int);
+void div_nodes(stack_t **, unsigned int);
+void mul_nodes(stack_t **, unsigned int);
+void mod_nodes(stack_t **, unsigned int);
+
+/*String operations*/
+void print_char(stack_t **, unsigned int);
+void print_str(stack_t **, unsigned int);
+void rotl(stack_t **, unsigned int);
+
+/*Error hanlding*/
+void err(int error_code, ...);
+void more_err(int error_code, ...);
+void string_err(int error_code, ...);
+void rotr(stack_t **, unsigned int);
 
 #endif
