df8627974230118ca69890a62e529e3917dfaf80
othmane <othmane.alaouimekkaoui@gmail.com>
2023-10-18 16:34:45 -0700

    test

--- errors.c
+++ errors.c
@@ -3,14 +3,6 @@
 /**
  * err - Prints appropiate error messages determined by their error code.
  * @error_code: The error codes are the following:
- * (1) => The user does not give any file or more than one file to the program.
- * (2) => The file provided is not a file that can be opened or read.
- * (3) => The file provided contains an invalid instruction.
- * (4) => When the program is unable to malloc more memory.
- * (5) => When the parameter passed to the instruction "push" is not an int.
- * (6) => When the stack it empty for pint.
- * (7) => When the stack it empty for pop.
- * (8) => When stack is too short for operation.
  */
 void err(int error_code, ...)
 {
@@ -49,10 +41,6 @@ void err(int error_code, ...)
 /**
  * more_err - handles errors.
  * @error_code: The error codes are the following:
- * (6) => When the stack it empty for pint.
- * (7) => When the stack it empty for pop.
- * (8) => When stack is too short for operation.
- * (9) => Division by zero.
  */
 void more_err(int error_code, ...)
 {
